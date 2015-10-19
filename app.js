e45bfb6e3b117c6b740796545f6b31dd40c2e748
Vipul Parmar <vrparmar@gmail.com>
2015-10-18 21:59:18 -0700

    Revert "testify added"
    This reverts commit 94b006f06d9a462d06ce6e7b17f5f2d89280e5c3.

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
-var restify = require('restify');
+var ws = require("nodejs-websocket");
 
 var port = process.env.PORT || 8001;
 
-var server = restify.createServer({
-  name: 'myapp',
-  version: '1.0.0'
-});
-server.use(restify.acceptParser(server.acceptable));
-server.use(restify.queryParser());
-server.use(restify.bodyParser());
+// Create a server connection
+var server = ws.createServer(function (conn) {
+    console.info("New connection....");
 
-server.get('/echo/:name', function (req, res, next) {
-  res.send(req.params);
-  return next();
-});
+    conn.on("text", function (str) {
+        console.log("Received "+str)
+        conn.sendText(str.toUpperCase()+"!!!")
+    })
 
-server.post('/msg', function create(req, res, next) {
-   console.log(req.body);
-   res.send(201, Math.random().toString(36).substr(3, 8));
-   return next();
- });
-
-server.listen(port, function () {
-  console.log('%s listening at %s', server.name, server.url);
-});
+    // Fires when client closes connection
+    conn.on("close", function (code, reason) {
+        console.info("Connection closed");
+    })
+}).listen(port);
